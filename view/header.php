6d1a9a930ab907982ce34aac631e0b9cf0ce4c15
nguyenthanhlam21 <lamn47593@gmail.com>
2023-11-20 15:21:21 +0700

    giao dien user va giao dien admin

---
+++ view/header.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>F-Watch</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+    
+    <header>
+        <div class="logo">
+            <a href="index.php"><img src="upload/Screenshot 2023-11-13 203703.png" alt=""></a>
+        </div>
+        <div class="menu">
+            <li><a href="index.php">Trang chủ</a></li>
+            <li><a href="index.php?pg=sanpham">Sản Phẩm</a></li>
+            <li><a href="">Giới Thiệu</a></li>
+            <li><a href="">Hỏi đáp</a></li>
+        </div>
+        <div class="others">
+            <li>
+                <form action="index.php?pg=sanpham" method="post">
+                    <input type="text" name="kyw" id="" placeholder="Nhap tu khoa tim kiem">
+                    <input class="submit" type="submit" name="timkiem" value="Tìm kiếm">
+                </form>
+            </li>
+            <li><a href="">Đăng ký</a></li>
+            <li><a href="">Đăng nhập</a></li>
+            <li><a href="">Giỏ hàng</a></li>
+        </div>
+    </header>
