a530d1faa8299784a725a153a336268d2431af93
hoangah <anhpbhph32332@fpt.edu.vn>
2023-11-23 11:23:17 +0700

    dk_dn

---
+++ dk_dn/dangky.php
@@ -0,0 +1,34 @@
+
+<section class="containerfull bg1 padd50">
+        <div class="container3">
+            <h1>ĐĂNG KÝ</h1>
+            <form action="index.php?pg=adduser" method="post" require_once>
+                            <div class="row mb10">
+                                Email <br>
+                                <input type="email" name="email" id="">
+                            </div>
+                            <div class="row mb10">
+                                Ten dang nhap <br>
+                                <input type="text" name="username">
+                            </div>
+                            <div class="row mb10">
+                                Mat khau <br>
+                                <input type="password" name="password">
+                            </div>
+                            <div class="row mb10">
+                                Số điện thoại <br>
+                                <input type="text" name="sdt" id="">
+                            </div>
+                            <div class="row mb10">
+                                <input type="checkbox">Tôi đồng ý với các điều khoản <a href="">người dùng</a> và<a href=""> bảo mật</a>
+                            </div>
+                            <div class="row mb10">
+                                <input type="submit" value="Dang ky" name="dangky">
+                            </div>
+                            <div class="row mb10">
+                                <input type="reset" value="Nhap lai">
+                            </div>
+                           
+
+        </div>
+</section>
